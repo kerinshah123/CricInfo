5b1d4f0a10f1c36bf9f29b55fb95359df1e1a828
keyurmistry <keyurmistry96@gmail.com>
2020-10-10 14:59:04 -0400

    create team,add player

---
+++ app/src/main/java/com/example/criinfo/More/Team.java
@@ -0,0 +1,64 @@
+package com.example.criinfo.More;
+
+import java.util.ArrayList;
+
+public class Team {
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSortname() {
+        return sortname;
+    }
+
+    public void setSortname(String sortname) {
+        this.sortname = sortname;
+    }
+
+    public ArrayList<String> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<String> players) {
+        this.players = players;
+    }
+
+    String city,country,image,managerId,name,sortname;
+    ArrayList<String> players;
+}
